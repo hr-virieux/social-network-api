8259bcc94f7a970c071f50cfa4d954be09356bc6
Henry Virieux <105490926+hr-virieux@users.noreply.github.com>
2024-04-01 00:19:43 +1100

    Update server.js

--- server.js
+++ server.js
@@ -16,4 +16,4 @@ app.use('/api/users', userRoutes);
 app.use('/api/thoughts', thoughtRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
