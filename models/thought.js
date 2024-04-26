ba3d5ef4300293154083fe3b2eb0ae87982316c2
Henry Virieux <105490926+hr-virieux@users.noreply.github.com>
2024-04-27 00:55:11 +1000

    Create thought.js

---
+++ models/thought.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+  reactionBody: { type: String, required: true, maxlength: 280 },
+  username: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+}, {
+  toJSON: { getters: true },
+  id: false
+});
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
+  createdAt: { type: Date, default: Date.now },
+  username: { type: String, required: true },
+  reactions: [reactionSchema]
+}, {
+  toJSON: { virtuals: true },
+  id: false
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+module.exports = mongoose.model('thought', thoughtSchema);
